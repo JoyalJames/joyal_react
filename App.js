bb8bd2fad77a1c3992f91434413fa805b6447874
Joyal <joyaljames100@gmail.com>
2024-02-01 10:32:45 +0530

    jsx

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//ReactElement
+// const heading = React.createElement("h1",{id:"heading"},"Hi Joyal Start");
+
+
+// JSX =>Babel transpiles it to React.createElement executed and to ReactElement-JS object is render on to HTML element(render)
+const jsxheading = <h1 id="heading" className="header">Hii Joo</h1>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading);
