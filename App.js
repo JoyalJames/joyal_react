37a7036670fe8641bec830421c778fc2275d1897
Joyal <joyaljames100@gmail.com>
2024-02-01 10:40:35 +0530

    Functional Component

--- App.js
+++ App.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom/client";
 
 //ReactElement
 // const heading = React.createElement("h1",{id:"heading"},"Hi Joyal Start");
-
-
 // JSX =>Babel transpiles it to React.createElement executed and to ReactElement-JS object is render on to HTML element(render)
-const jsxheading = <h1 id="heading" className="header">Hii Joo</h1>
+//REACT FUNCTIONAL COMPONENT
+const Title=()=>(
+    <h1>Namste Joyal</h1>
+)
+const element = "Namsteeee"
+const JsxHead =()=>(
+    <div>{element}
+        <Title/>
+        {Title()}
+        <h1 id="heading" className="header">Hii Joo</h1>
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
+root.render(<JsxHead/>);
