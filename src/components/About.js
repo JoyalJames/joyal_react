eb58f7461a6f6df3c437effed057e7c0a2a0688c
Joyal <joyaljames100@gmail.com>
2024-02-11 10:51:54 +0530

    API componentDidMount()

--- src/components/About.js
+++ src/components/About.js
@@ -5,33 +5,24 @@ class About extends Component{
     // 1st Contructor will load   -> then render component  -> then ComponentDidMount()
     constructor(props){
         super(props)
-        console.log("Parent Constructor")
+        // console.log("Parent Constructor")
     }
     // 3rd componentDidMount()
     componentDidMount(){
-        console.log("Parent Component Did Mount")
+        // console.log("Parent Component Did Mount")
     }
     // 2nd render Component
     render(){
-        console.log("Parent Render")
+        // console.log("Parent Render")
         return (
             <div className="about">
                 <h1>About Page</h1>
                 <h2>Welcome To Joyals Page</h2>
                 {/* <User/> */}
-                <UserClass name={"Joyal"}/>
-                <UserClass name={"Hello"}/>
+                <UserClass/>
             </div>
         )
     }
 }
-//  ->Parent Constructor
-//      ->Parent Render
-//          ->JoyalChild Constructor
-//          ->JoyalChild Render
-//          ->HelloChild Constructor
-//          ->HelloChild Render
-//  ->JoyalChild Component Did Mount
-//  ->HelloChild Component Did Mount
-//-> Parent Component Did Mount
+
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,26 +2,40 @@ import React from "react"
 class UserClass extends React.Component {
     constructor(props){
         super(props);
-        console.log(this.props.name + "Child Constructor")
-        this.state = {
-            count : 0,
-            count2 : 1
-        };
+        // console.log(this.props.name + "Child Constructor")
+        this.state={
+            useInfo:{
+                name:"Dummy",
+                location:"Default",
+                avatar_url :""
+            }
+        }
     }
-    componentDidMount(){
-        console.log(this.props.name + "Child Component Did Mount")
+    async componentDidMount(){
+        // console.log(this.props.name + "Child Component Did Mount")
+
+        const data = await fetch("https://api.github.com/users/akshaymarch7");
+        const json = await data.json();
+        console.log(json)
+
+        this.setState({
+            useInfo:json,
+        })
+    }
+    componentDidUpdate(){
+
+    }
+    componentWillUnmount(){
+        
     }
     render(){
-        console.log(this.props.name + "Child Render")
+        // console.log(this.props.name + "Child Render")
         return(
             <div className="about_user">
                 <h1>{this.state.count}</h1>
-                <button onClick={()=>{
-                    this.setState({
-                        count : this.state.count + 1
-                    })
-                }}>Count +</button>
-                <h1>Name  :  {this.props.name}</h1>
+                
+                <h1>Name  :  {this.state.useInfo.name}</h1>
+                <img src={this.state.useInfo.avatar_url}/>
                 <h2>Location  :  Kottayam</h2>
                 <h2>Contact  :  @joyaljames</h2>
             </div>
